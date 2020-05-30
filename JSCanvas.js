f091dcd693824554403a97765319b999ea0b90ac
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-05-30 10:48:06 -0700

    Class setup with rectangles and draw function

--- JSCanvas.js
+++ JSCanvas.js
@@ -3,7 +3,11 @@ class Canvas
 	constructor(canvas)
 	{
 		this.canv = canvas;
+		this.ctx = canvas.getContext("2d");
 		this.updateMetadata();
+		this.restoreDefaults();
+		//Start up requestAnimationFrame
+		//Start up ticking clock
 	}
 
 	updateMetadata()
@@ -12,13 +16,52 @@ class Canvas
 		this.width = this.canv.width;
 	}
 
-	get height()
+	restoreDefaults()
 	{
-		return this.height;
+		this.ctx.fillColor = "#000000";
+		this.ctx.font = "10px Arial";
 	}
 
-	get width()
+	draw(shape, ...args)
 	{
-		return this.width;
+		shape = shape.toUpperCase().trim();
+		switch(shape)
+		{
+			case "RECT":
+				console.log("RECT");
+				rect(...args);
+			break;
+			case "CIRC":
+				console.log("CIRC");
+			break;
+			case "LINE":
+				console.log("LINE");
+			break;
+			default:
+				console.log("Invalid shape input entered");
+			break;
+		}
 	}
+
+	rect(x, y, width, height, color, method)
+	{
+		this.setColor(color);
+		this.ctx.fillRect(x, y, width, height);
+	}
+
+	setColor(color)
+	{
+		color = color.toUpperCase();
+		if(color && color.match(/^#([0-9A-Z]{3}){1,2}$/))
+		{
+			this.ctx.fillStyle = color;
+		}
+		else
+		{
+			console.log("Invalid color format. Use Hexidecimal.");
+		}
+	}
+
+	get height() { return this.height; }
+	get width(){ return this.width; }
 }
