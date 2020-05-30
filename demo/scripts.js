3ddd4f282ddcaf373d4156206b9743b313727fb5
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-05-29 19:08:20 -0700

    Initial files

---
+++ demo/scripts.js
@@ -0,0 +1,7 @@
+const canv = document.getElementById("canv");
+const ctx = canv.getContext("2d");
+var mode = "start";
+
+ctx.clearRect(0,0,canv.width,canv.height);
+ctx.font="10px Arial";
+ctx.fillStyle="#FFFFFF";
