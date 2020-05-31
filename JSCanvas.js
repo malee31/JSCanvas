7286b56b5a05eeb75576da62847a2d92ccb63e34
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-05-30 20:48:55 -0700

    Set up more basic functions. Scaling fixed for the canvas

--- JSCanvas.js
+++ JSCanvas.js
@@ -1,25 +1,63 @@
-class Canvas
+class JSCanvas
 {
-	constructor(canvas)
+	constructor(canv)
 	{
-		this.canv = canvas;
-		this.ctx = canvas.getContext("2d");
+		this.canvas = canv;
+		this.canvas.addEventListener("resize", () => {
+			console.log("Resized");
+		});
+		this.ctx = canv.getContext("2d");
+		this.restoreAllDefaults();
 		this.updateMetadata();
-		this.restoreDefaults();
-		//Start up requestAnimationFrame
-		//Start up ticking clock
 	}
 
 	updateMetadata()
 	{
-		this.height = this.canv.height;
-		this.width = this.canv.width;
+		//Fixes Aspect Ratio
+		this.canvas.width = this.canvas.height * (this.canvas.clientWidth / this.canvas.clientHeight);
+		this.canvas.width *= this.resolutionScale;
+		this.canvas.height *= this.resolutionScale;
 	}
 
 	restoreDefaults()
 	{
 		this.ctx.fillColor = "#000000";
 		this.ctx.font = "10px Arial";
+		this.resolutionScale = 10;
+	}
+
+	restoreAllDefaults()
+	{
+		this.restoreDefaults();
+		this.toggle = false;
+		this.resetTimers();
+	}
+
+	resetTimers()
+	{
+		this.counter = 0;
+		this.timeout = 0;
+		//Results in 9000000000000000
+		this.timerLoop = Number.MAX_SAFE_INTEGER - 7199254740991;
+		requestAnimationFrame(this.sanityCheck.bind(this));
+		this.timer = setInterval(this.tick.bind(this), 500);
+	}
+
+	sanityCheck()
+	{
+		if(this.timeout > 0)
+		{
+			setTimeout(requestAnimationFrame, this.timeout, this.sanityCheck.bind(this));
+		}
+		else
+		{
+			requestAnimationFrame(this.sanityCheck.bind(this));
+		}
+	}
+
+	tick()
+	{
+		this.counter = (this.counter + 1) % this.timerLoop;
 	}
 
 	draw(shape, ...args)
@@ -28,40 +66,70 @@ class Canvas
 		switch(shape)
 		{
 			case "RECT":
-				console.log("RECT");
+				this.verboseLog("RECT");
 				rect(...args);
 			break;
 			case "CIRC":
-				console.log("CIRC");
+				this.verboseLog("CIRC");
 			break;
 			case "LINE":
-				console.log("LINE");
+				this.verboseLog("LINE");
 			break;
 			default:
-				console.log("Invalid shape input entered");
+				this.verboseLog("Invalid shape input entered");
 			break;
 		}
 	}
 
 	rect(x, y, width, height, color, method)
 	{
-		this.setColor(color);
+		this.fillColor(color);
 		this.ctx.fillRect(x, y, width, height);
 	}
 
-	setColor(color)
+	fill(color)
 	{
-		color = color.toUpperCase();
+		color = color ? color.toUpperCase() : null;
+		this.verboseLog(color);
 		if(color && color.match(/^#([0-9A-Z]{3}){1,2}$/))
 		{
+			this.verboseLog("Color set successfully");
 			this.ctx.fillStyle = color;
 		}
 		else
 		{
-			console.log("Invalid color format. Use Hexidecimal.");
+			this.verboseLog("Invalid color format. Use Hexidecimal.");
 		}
 	}
 
-	get height() { return this.height; }
-	get width(){ return this.width; }
+	get height() { return this.canvas.height; }
+	get width(){ return this.canvas.width; }
+
+	//Aliases
+	fillColor(...args) { this.fill(...args); }
+
+	//Debug mode
+	toggleVerbose(toggle)
+	{
+		if(typeof toggle == "boolean") this.toggle = toggle;
+		else this.toggle = !this.toggle;
+	}
+
+	verboseLog(message, ...additional)
+	{
+		if(this.toggle)
+		{
+			switch(additional.length)
+			{
+				case 0:
+					console.log(message);
+				break;
+				case 1:
+					console.log(message, additional[0]);
+				break;
+				default:
+					console.log(message, additional);
+			}
+		}
+	}
 }
--- demo/scripts.js
+++ demo/scripts.js
@@ -1,7 +1,5 @@
-const canv = document.getElementById("canv");
-const ctx = canv.getContext("2d");
+const JSCanv = new JSCanvas(document.getElementById("canv"));
+JSCanv.toggleVerbose(true);
 var mode = "start";
 
-ctx.clearRect(0,0,canv.width,canv.height);
-ctx.font="10px Arial";
-ctx.fillStyle="#FFFFFF";
+JSCanv.rect(0, 0, 500, 500, "#000");
