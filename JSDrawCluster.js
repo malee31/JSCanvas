ea4ab2ec886661f8cb496093e0a28e8d67b96876
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-06-14 17:33:49 -0700

    JSDoc'ed JSDrawCluster and added moving for the draw clusters

--- JSDrawCluster.js
+++ JSDrawCluster.js
@@ -8,20 +8,31 @@ class JSDrawCluster
 		this.drawObjects = [];
 	}
 
+	/**
+	 * Adds a new shape array into the draw group
+	 */
 	add(obj)
 	{
 		drawObjects.push(obj);
 	}
 
+	/**
+	 * Removes all shapes from the draw group
+	 */
 	clear()
 	{
 		drawObjects = [];
 	}
 
+	/**
+	 * Shifts the draw group's center's location by x and y
+	 * @param {number} [x] The number of pixels to shift in the X direction
+	 * @param {number} [y] The number of pixels to shift in the Y direction
+	 */
 	shiftCenter(x, y)
 	{
-		this.shift[0] += x;
-		this.shift[1] += y;
+		if(typeof x == "number") this.drawCenter[0] += x;
+		if(typeof y == "number") this.drawCenter[1] += y;
 	}
 
 	shiftAll()
@@ -29,19 +40,32 @@ class JSDrawCluster
 		var shifted = [];
 		for(let drawObj in this.drawObjects)
 		{
-			var shiftedInstance = drawObj.slice();
+			var shiftedInstance = inputArrayCopy(drawObj);
 			//TODO: Shift X and Y by this.shift[0] and this.shift[1] respectively
+			for(var pos = 0; pos < Math.min(shiftedInstance[1].length, shiftedInstance[2].length); pos++)
+			{
+				shiftedInstance[1][pos] += this.shift[0] - this.drawCenter[0];
+				shiftedInstance[2][pos] += this.shift[1] - this.drawCenter[1];
+			}
 			shifted.push(shiftedInstance);
 		}
 		return shifted;
 	}
 
+	/**
+	 * Translates the entire draw group
+	 * @param {number} [x] The number of pixels to shift in the X direction
+	 * @param {number} [y] The number of pixels to shift in the Y direction
+	 */
 	translate(x, y)
 	{
-		this.drawCenter[0] += x;
-		this.drawCenter[1] += y;
+		if(typeof x == "number") this.shift[0] += x;
+		if(typeof y == "number") this.shift[1] += y;
 	}
 
+	/**
+	 * Draws the draw group in its location after translating all the shapes it is made out of
+	 */
 	draw()
 	{
 		var shifted = this.shiftAll();
@@ -51,6 +75,22 @@ class JSDrawCluster
 			else if(typeof this.JSCanvas.draw = "function") this.JSCanvas.draw(drawObj);
 			else console.log("ERROR: No JSCanvas set. Cannot find draw function");
 		}
+	}
+
+	/**
+	 * Makes a shallow copy of an array with a depth of two
+	 * @param {Array} arr Array to be copied
+	 * @param {Array} [base] Array to push a copy of arr into
+	 */
+	inputArrayCopy(arr, base)
+	{
+		var rebuild = Array.isArray(base) ? base : [];
+		for(let item of arr)
+		{
+			if(Array.isArray(item)) rebuild.push(item.slice());
+			else rebuild.push(item);
+		}
+		return rebuild;
 	}
 
 	move(x, y) { translate(x, y); }
