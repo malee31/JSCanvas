2ce3e39ca38d88c3d42af4c4a32c5cec8e7c975a
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-06-19 23:10:58 -0700

    Now tracking mouse angle relative to center where 0 degrees is East in collisionDemo

--- demos/collisionDemo/scripts.js
+++ demos/collisionDemo/scripts.js
@@ -1,5 +1,12 @@
 const JSCanv = new JSMemoryCanvas(document.getElementById("canv"));
 
 JSCanv.setAction(() => {
-	if(JSCanv.mouse.mouseDown) JSCanv.circ(JSCanv.cursor.x, JSCanv.cursor.y, 20, "#000");
+	JSCanv.circ(JSCanv.cursor.x, JSCanv.cursor.y, 20, "#000");
+	var x = JSCanv.cursor.x - JSCanv.width / 2;
+	var y = JSCanv.cursor.y - JSCanv.height / 2;
+	var angle = Collider.toDegrees(Math.atan(y / x));
+	angle *= -1;
+	if(x < 0) angle += 180;
+	else if(y > 0) angle += 360;
+	console.log(angle);
 });
