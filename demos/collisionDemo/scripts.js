aa5ce8616f089b0165b4c5fe6691b0e2ad1ad28d
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-06-20 19:38:34 -0700

    Line tracking The mouse crossing the center created

--- demos/collisionDemo/scripts.js
+++ demos/collisionDemo/scripts.js
@@ -1,6 +1,7 @@
-const JSCanv = new JSMemoryCanvas(document.getElementById("canv"));
+const JSCanv = new JSCanvas(document.getElementById("canv"));
 
 JSCanv.setAction(() => {
+	JSCanv.clear();
 	JSCanv.circ(JSCanv.cursor.x, JSCanv.cursor.y, 20, "#000");
 	var x = JSCanv.cursor.x - JSCanv.width / 2;
 	var y = JSCanv.cursor.y - JSCanv.height / 2;
@@ -8,5 +9,20 @@ JSCanv.setAction(() => {
 	angle *= -1;
 	if(x < 0) angle += 180;
 	else if(y > 0) angle += 360;
-	console.log(angle);
+	//console.log(angle);
+
+	var tan = Math.tan(Collider.toRadians(angle));
+	var cent = {x: JSCanv.width / 2, y: JSCanv.height / 2};
+	JSCanv.strokeSize = 10;
+	var plusMinus = 0;
+	if((angle > 45 && angle < 135) || (angle > 225 && angle < 315))
+	{
+		var xInter = cent.y / tan;
+		JSCanv.line([cent.x + xInter, cent.x - xInter], [0, JSCanv.height], "#000");
+	}
+	else
+	{
+		var yInter = cent.x * tan;
+		JSCanv.line([0, JSCanv.width], [cent.y + yInter, cent.y - yInter], "#000");
+	}
 });
