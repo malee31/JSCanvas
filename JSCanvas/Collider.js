326cf7216ab6e7d86de1b41ba05cb5ed025d3612
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-06-22 23:47:11 -0700

    Moved atan function into Collider.js rather than as a separate script

--- JSCanvas/Collider.js
+++ JSCanvas/Collider.js
@@ -1,4 +1,3 @@
-
 /** Class containing functions to test for collisions using SAT */
 class Collider
 {
@@ -44,11 +43,15 @@ class Collider
 	 * @param {number[]} yCoords The Y positions of the sets of points to rotate
 	 * @param {number} angle The angle to rotate all the points by
 	 * @param {boolean} [isRadians] Whether the angle input is in radians. Will convert from degrees otherwise.
+	 * @param {number} [centerX] X position of the rotational origin. Defaults to 0
+	 * @param {number} [centerY] Y position of the rotational origin. Defaults to 0
 	 * @returns {PointSet} Returns the rotated points in the form of an object
 	 */
-	static rotatePoints(xCoords, yCoords, angle, isRadians)
+	static rotatePoints(xCoords, yCoords, angle, isRadians, centerX, centerY)
 	{
 		angle = isRadians ? angle : this.toRadians(angle);
+		centerX = centerX ? centerX : 0;
+		centerY = centerY ? centerY : 0;
 		var length = Math.min(xCoords.length, yCoords.length);
 		var cosAngle = Math.cos(angle);
 		var sinAngle = Math.sin(angle);
@@ -56,10 +59,33 @@ class Collider
 		yCoords = yCoords.splice(0, length);
 		for(let coord = 0; coord < length; coord++)
 		{
+			xCoords -= centerX;
+			yCoords -= centerY;
 			xCoords[coord] =  xCoords[coord] * cosAngle - yCoords[coord] * sinAngle;
 			yCoords[coord] =  xCoords[coord] * sinAngle + yCoords[coord] * cosAngle;
+			xCoords += centerX;
+			yCoords += centerY;
 		}
 		return {x: xCoords, y: yCoords};
+	}
+
+	/**
+	 * Computes the arctangent of a slope relative to 3 o'clock counterclockwise
+	 * Able to return a range between 0 and 360 (excludes 360)
+	 * @param {number} x The change in X to calculate the slope and atan with relative to the origin
+	 * @param {number} y The change in Y to calculate the slope and atan with relative to the origin
+	 * @param {boolean} [toDegrees] Whether or not to return a value in degrees or radians. Defaults to degrees
+	 * @returns {number} The full arctangent between 0 and 360 degrees (excluding 360) or its equivalent in radians 
+	 *
+	 */
+	static fullAtan(x, y, toDegrees)
+	{
+		var angle = Collider.toDegrees(Math.atan(y / x));
+		angle *= -1;
+		if(x < 0) angle += 180;
+		else if(y > 0) angle += 360;
+		if(toDegrees === false) angle = Collider.toRadians(angle);
+		return angle;
 	}
 
 	/**
--- demos/collisionDemo/scripts.js
+++ demos/collisionDemo/scripts.js
@@ -6,11 +6,8 @@ JSCanv.setAction(() => {
 	JSCanv.circ(JSCanv.cursor.x, JSCanv.cursor.y, 20, "#000");
 	var x = JSCanv.cursor.x - JSCanv.width / 2;
 	var y = JSCanv.cursor.y - JSCanv.height / 2;
-	var angle = Collider.toDegrees(Math.atan(y / x));
-	angle *= -1;
-	if(x < 0) angle += 180;
-	else if(y > 0) angle += 360;
-	//console.log(angle);
+	var angle = Collider.fullAtan(x, y);
+	console.log(angle);
 
 	var tan = Math.tan(Collider.toRadians(angle));
 	var cent = {x: JSCanv.width / 2, y: JSCanv.height / 2};
